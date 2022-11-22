b5cae21e345db62628e7fb13e4d51eb2c3b0575d
Vishnu <vishnusr01@gmail.com>
2022-11-23 06:52:14 +0800

    Initial project setup

---
+++ src/main/java/com/example/SpringbootJooqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJooqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJooqDemoApplication.class, args);
+	}
+
+}
